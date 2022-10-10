3f17d5a34d8259aac3e3ab6ffbb9decedb53ae5a
Jeremy Boxerman <jboxerman08@gmail.com>
2022-10-10 09:19:34 -0400

    updated personal projects section, added lewagon diff video, responsiveness WIP

--- scripts.js
+++ scripts.js
@@ -3,3 +3,15 @@
 // switchElement.addEventListener('click', () => {
 //   document.body.classList.toggle('dark')
 // })
+
+
+// When the user scrolls down passed the hero banner, make background of header change color
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("header").style.backgroundColor = "#222831";
+  } else {
+    document.getElementById("header").style.backgroundColor = "transparent";
+  }
+}
