657680c3ede595cbdc5412c8bc60c223b6a99f05
Jeremy Boxerman <jboxerman08@gmail.com>
2022-10-14 17:11:16 -0400

    added blender images to gallery, updated school and personal projects section

--- scripts.js
+++ scripts.js
@@ -19,28 +19,28 @@ function scrollFunction() {
 
 
 // Image gallery
-const current = document.querySelector('#current');
-const imgs = document.querySelector('.imgs');
-const img = document.querySelectorAll('.imgs img');
+const currentImg = document.querySelector('#current-img');
+const imageGrid = document.querySelector('.personal-projects__image-grid');
+const img = document.querySelectorAll('.personal-projects__image-grid img');
 const opacity = 0.6;
 
 // Set first img opacity
 img[0].style.opacity = opacity;
 
-imgs.addEventListener('click', imgClick);
+imageGrid.addEventListener('click', imgClick);
 
 function imgClick(e) {
   // Reset the opacity
   img.forEach(img => (img.style.opacity = 1));
 
   // Change current image to src of clicked image
-  current.src = e.target.src;
+  currentImg.src = e.target.src;
 
   // Add fade in class
-  current.classList.add('fade-in');
+  currentImg.classList.add('fade-in');
 
   // Remove fade-in class after .5 seconds
-  setTimeout(() => current.classList.remove('fade-in'), 500);
+  setTimeout(() => currentImg.classList.remove('fade-in'), 500);
 
   // Change the opacity to opacity var
   e.target.style.opacity = opacity;
