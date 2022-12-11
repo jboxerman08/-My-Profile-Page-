77d4a4c0e5ca40e99085dbe4278f03d4d0090197
Jeremy Boxerman <jboxerman08@gmail.com>
2022-12-10 22:53:41 -0500

    updated html to and css to fix sticky navbar issue

--- scripts.js
+++ scripts.js
@@ -5,9 +5,6 @@
 //   document.body.classList.toggle('dark')
 // })
 
-
-const navbar = document.querySelector(".navbar");
-const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll('section');
 const navbarLinks = document.querySelectorAll('.navbar-link');
 
